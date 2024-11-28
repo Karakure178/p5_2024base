ac3a5d31d9c6a174a4af18a3875ce062d50b997a
karakure178 <karakure1780@gmail.com>
2024-11-28 23:07:28 +0900

    add - ファイル移動

---
+++ src/dev/rect-rotate/js/libs/sketch.js
@@ -0,0 +1,88 @@
+import gsap from "gsap";
+import fs from "./shader/normal.frag";
+import vs from "./shader/normal.vert";
+
+/**
+ * shader&base テンプレ
+ * @param {p5} p - The p5.js instance.
+ */
+export const sketch = (p) => {
+  let canvas;
+  let pg;
+  let theShader1;
+
+  p.setup = () => {
+    const init = () => {
+      const canvasid = document.getElementById("mycanvas");
+      canvas = p.createCanvas(
+        canvasid.clientWidth,
+        canvasid.clientHeight,
+        p.WEBGL
+      );
+      canvas.parent(canvasid);
+      p.imageMode(p.CENTER);
+      p.textureMode(p.NORMAL);
+      p.frameRate(24);
+      p.noStroke();
+    };
+    init();
+
+    pg = p.createGraphics(p.width, p.height);
+    image_init(pg, p);
+
+    theShader1 = p.createShader(vs, fs);
+  };
+
+  p.draw = () => {
+    p.background(110);
+
+    p.push();
+    const rect_s = 100;
+    pg.push();
+    pg.translate(pg.width / 2, pg.height / 2);
+    pg.rect(0, 0, rect_s, rect_s);
+    pg.pop();
+
+    const shaderImage = () => {
+      p.shader(theShader1);
+      theShader1.setUniform(`u_tex`, pg);
+      theShader1.setUniform("u_resolution", [pg.width, pg.height]);
+      theShader1.setUniform(`u_time`, -p.frameCount / 35);
+    };
+    //shaderImage();
+
+    //p.image(pg, 0, 0);
+    base(p, pg);
+    p.pop();
+  };
+
+  p.keyPressed = () => {
+    if (p.key === "s") {
+      p.saveCanvas(canvas, "image", "png");
+      p.saveGif("image", 4);
+    }
+  };
+};
+
+/** pgの初期化関数
+ * @function image_init
+ * @param {p5.Graphics} pg - p5.Graphics
+ * @param {p5.canvas} p - p5インスタンス
+ */
+const image_init = (pg, p) => {
+  pg.rectMode(p.CENTER);
+  pg.background(110);
+  pg.fill(255);
+  pg.noStroke();
+};
+
+const base = (p, pg) => {
+  const r = 100;
+  const n = 100;
+  const angle = 360 / n;
+  for (let i = 0; i < n; i++) {
+    let x = r * p.cos(p.radians(angle * i));
+    let y = r * p.sin(p.radians(angle * i));
+    p.rect(x, y, 5, 5);
+  }
+};

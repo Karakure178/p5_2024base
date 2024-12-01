c2efe1d89a914ce9391ffb98382c05b77a8bc684
karakure178 <karakure1780@gmail.com>
2024-12-01 16:26:31 +0900

    add - ぐるぐる追加

---
+++ src/template/base6/js/libs/sketch.js
@@ -0,0 +1,82 @@
+import gsap from "gsap";
+
+/**
+ * shader&base テンプレ
+ * @param {p5} p - The p5.js instance.
+ */
+export const sketch = (p) => {
+  let canvas;
+  let pg;
+
+  p.setup = () => {
+    const init = () => {
+      const canvasid = document.getElementById("mycanvas");
+      canvas = p.createCanvas(
+        canvasid.clientWidth,
+        canvasid.clientHeight,
+        p.WEBGL
+      );
+      canvas.parent(canvasid);
+      p.imageMode(p.CENTER);
+      p.textureMode(p.NORMAL);
+      p.frameRate(24);
+
+      p.noStroke();
+    };
+    init();
+
+    pg = p.createGraphics(p.width, p.height);
+    image_init(pg, p);
+  };
+
+  p.draw = () => {
+    p.background(110);
+    p.push();
+
+    pg.push();
+    pg.translate(pg.width / 2, pg.height / 2);
+    for (let i = 0; i < 500; i++) {
+      pg.rotate(p.radians(p.random(0, 90)));
+      base(p, pg, i);
+    }
+    pg.pop();
+
+    p.image(pg, 0, 0);
+    p.pop();
+    p.noLoop();
+  };
+
+  p.keyPressed = () => {
+    if (p.key === "s") {
+      p.saveCanvas(canvas, "image", "png");
+      p.saveGif("image", 4);
+    }
+  };
+};
+
+/** pgの初期化関数
+ * @function image_init
+ * @param {p5.Graphics} pg - p5.Graphics
+ * @param {p5.canvas} p - p5インスタンス
+ */
+const image_init = (pg, p) => {
+  pg.rectMode(p.CENTER);
+  pg.background(110);
+  // pg.fill(255);
+  pg.noFill();
+};
+
+const base = (p, pg, r) => {
+  const rands = [60, 90, 120, 150, 180, 210, 270, 300];
+  const rand = Math.floor(p.random(rands));
+  pg.beginShape();
+  for (let i = 0; i < 360; i++) {
+    if (rand === i) {
+      pg.endShape();
+      return;
+    }
+    let x = r * p.cos(p.radians(i));
+    let y = r * p.sin(p.radians(i));
+    pg.vertex(x, y);
+  }
+};
